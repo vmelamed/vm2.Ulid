c00c30296e78d750a2cfa2f6b35fdfc63a225971
Val Melamed <valo@vtmelamed.com>
2025-09-21 01:20:02 -0400

    Refactor UlidNsConverter for generalized JSON conversion
    Refactored the `UlidNsConverter` class to inherit from `JsonConverter` instead of `JsonConverter<Nullable<Ulid>>`.

--- src/UlidType/NsJson/UlidNsConverter.cs
+++ src/UlidType/NsJson/UlidNsConverter.cs
@@ -9,23 +9,51 @@ using Newtonsoft.Json;
 /// <remarks>This converter is used to serialize and deserialize <see cref="Ulid"/> values in JSON format. It
 /// ensures that <see cref="Ulid"/> instances are correctly represented as strings in JSON and parsed back into <see
 /// cref="Ulid"/> objects during deserialization.</remarks>
-public class UlidNsConverter : JsonConverter<Nullable<Ulid>>
+public class UlidNsConverter : JsonConverter
 {
     /// <summary>
-    /// Reads a JSON value and converts it into an instance of <see cref="Ulid"/>.
+    /// Determines whether the specified type can be converted to or from a <see cref="Ulid"/>.
     /// </summary>
-    /// <param name="reader">The <see cref="JsonReader"/> used to read the JSON data.</param>
-    /// <param name="_">The type of the object to deserialize. This parameter is ignored in this implementation.</param>
-    /// <param name="__">The existing <see cref="Ulid"/> value, if any. This parameter is ignored in this implementation.</param>
-    /// <param name="___">A value indicating whether an existing value is provided. This parameter is ignored in this implementation.</param>
-    /// <param name="____">The <see cref="JsonSerializer"/> used for deserialization.</param>
-    /// <returns>The deserialized <see cref="Ulid"/> instance.</returns>
-    public override Ulid? ReadJson(
-        JsonReader reader,
-        Type _,
-        Ulid? __,
-        bool ___,
-        JsonSerializer ____)
+    /// <param name="objectType">The type to evaluate for conversion compatibility.</param>
+    /// <returns>
+    /// <see langword="true"/> if the specified type is <see cref="Ulid"/> or <see cref="Nullable{Ulid}"/>; otherwise, <see langword="false"/>.
+    /// </returns>
+    public override bool CanConvert(Type objectType)
+        => objectType == typeof(Ulid) || objectType == typeof(Ulid?);
+
+    /// <summary>
+    /// Writes the JSON representation of the specified object using the provided <see cref="JsonWriter"/>.
+    /// </summary>
+    /// <param name="writer">The <see cref="JsonWriter"/> used to write the JSON output. Cannot be <c>null</c>.</param>
+    /// <param name="value">The object to serialize. Can be <c>null</c>, in which case a JSON null value is written.</param>
+    /// <param name="serializer">The <see cref="JsonSerializer"/> used to customize the serialization process. Cannot be <c>null</c>.</param>
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value is null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        if (value is Ulid ulid)
+        {
+            writer.WriteValue(ulid.ToString());
+            return;
+        }
+
+        throw new JsonWriterException($"Expected value to be of type {typeof(Ulid)} or null, but got {value.GetType()}.");
+    }
+
+    /// <summary>
+    /// Reads JSON data from the specified <see cref="JsonReader"/> and converts it into an object of the specified
+    /// type.
+    /// </summary>
+    /// <param name="reader">The <see cref="JsonReader"/> to read JSON data from.</param>
+    /// <param name="objectType">The type of the object to deserialize the JSON data into.</param>
+    /// <param name="existingValue">An existing object to populate with the JSON data, or <c>null</c> to create a new object.</param>
+    /// <param name="serializer">The <see cref="JsonSerializer"/> used to deserialize the JSON data.</param>
+    /// <returns>The deserialized object, or <c>null</c> if the JSON data is empty or cannot be deserialized.</returns>
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
     {
         try
         {
@@ -43,25 +71,5 @@ public class UlidNsConverter : JsonConverter<Nullable<Ulid>>
         {
             throw new JsonReaderException("Could not parse ULID value.", ex);
         }
-    }
-
-    /// <summary>
-    /// Writes a ULID value to a JSON stream using the specified <see cref="JsonWriter"/>.
-    /// </summary>
-    /// <param name="writer">The <see cref="JsonWriter"/> used to write the JSON output. Cannot be <see langword="null"/>.</param>
-    /// <param name="value">The <see cref="Ulid"/> value to write. Cannot be <see langword="null"/>.</param>
-    /// <param name="serializer">The <see cref="JsonSerializer"/> used to customize the serialization process. Cannot be <see langword="null"/>.</param>
-    public override void WriteJson(
-        JsonWriter writer,
-        Ulid? value,
-        JsonSerializer serializer)
-    {
-        if (value is null)
-        {
-            writer.WriteNull();
-            return;
-        }
-
-        writer.WriteValue(value.Value.ToString());
     }
 }
