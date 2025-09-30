ea39a8647550e2de73ad9278836ea5bbe784762e
Val Melamed <valo@vtmelamed.com>
2025-09-30 17:32:28 -0400

    debug+

--- scripts/bash/run-tests.sh
+++ scripts/bash/run-tests.sh
@@ -18,6 +18,8 @@ declare -x COVERAGE_RESULTS_DIR="$ARTIFACTS_DIR/CoverageResults"
 source "$script_dir/_common.sh"
 source "$script_dir/run-test-utils.sh"
 
+verbose=${VERBOSE:-true}
+quiet=${QUIET:-true}
 dump_vars \
     --header "Script Arguments:" \
     test_project \
@@ -33,6 +35,8 @@ dump_vars \
     solution_dir \
     script_dir \
     COVERAGE_RESULTS_DIR
+verbose=${VERBOSE:-false}
+quiet=${QUIET:-false}
 
 get_arguments "$@"
 
