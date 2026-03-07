d25b89a1418ea31bdf30c11f00a5dd0f544c7bf2
Val Melamed <valo@vtmelamed.com>
2026-03-07 08:51:42 -0500

    refactor: improve benchmark setup for GUID baseline and ULID parsing

--- benchmarks/UlidType.Benchmarks/UlidBenchmarks.cs
+++ benchmarks/UlidType.Benchmarks/UlidBenchmarks.cs
@@ -121,30 +121,32 @@ public class UlidToString
 public class ParseUlid
 {
     const int MaxDataItems = 1000;
-    PreGeneratedData<string> _data1 = null!;
     PreGeneratedData<string> _data2 = null!;
     PreGeneratedData<byte[]> _data3 = null!;
+#if GUID_BASELINE
+    PreGeneratedData<string> _data1 = null!;
+#endif
 
     [GlobalSetup]
     public void Setup()
     {
         UlidFactory _factory = new();
 
+        _data2 = new(MaxDataItems, _ => _factory.NewUlid().ToString());
+        _data3 = new(MaxDataItems, _ => Encoding.UTF8.GetBytes(_factory.NewUlid().ToString()));
 #if GUID_BASELINE
         _data1 = new(MaxDataItems, _ => Guid.NewGuid().ToString());
 #endif
-        _data2 = new(MaxDataItems, _ => _factory.NewUlid().ToString());
-        _data3 = new(MaxDataItems, _ => Encoding.UTF8.GetBytes(_factory.NewUlid().ToString()));
     }
-
-#if GUID_BASELINE
-    [Benchmark(Description = "Guid.Parse(string)", Baseline = true)]
-    public Guid Guid_Parse() => Guid.Parse(_data1.GetNext());
-#endif
 
     [Benchmark(Description = "Ulid.Parse(StringUtf16)")]
     public Ulid Ulid_Parse_Utf16() => Ulid.Parse(_data2.GetNext());
 
     [Benchmark(Description = "Ulid.Parse(StringUtf8)")]
     public Ulid Ulid_Parse_Utf8() => Ulid.Parse(_data3.GetNext());
+
+#if GUID_BASELINE
+    [Benchmark(Description = "Guid.Parse(string)", Baseline = true)]
+    public Guid Guid_Parse() => Guid.Parse(_data1.GetNext());
+#endif
 }
