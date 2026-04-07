ead0aad351fa0f9de3be267b11f3f97d74446720
Val Melamed <valo@vtmelamed.com>
2026-04-07 10:13:22 -0400

    chore: update copyright year to 2025-2026 in all files

--- src/UlidType/usings.cs
+++ src/UlidType/usings.cs
@@ -1,5 +1,5 @@
 ﻿// SPDX-License-Identifier: MIT
-// Copyright (c) 2025 Val Melamed
+// Copyright (c) 2025-2026 Val Melamed
 
 global using System;
 global using System.Diagnostics;
@@ -7,6 +7,15 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Numerics;
 global using System.Security.Cryptography;
 global using System.Text.Json;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Numerics;
+global using System.Runtime.Serialization;
+
+global using BenchmarkDotNet.Attributes;
+
+global using vm2;
+global using vm2.SemVerSerialization.NsJson;
+global using vm2.SemVerSerialization.SysJson;
 
 global using vm2.UlidSerialization.NsJson;
 global using vm2.UlidSerialization.SysJson;
