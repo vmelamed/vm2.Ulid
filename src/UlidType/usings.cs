a0022505decbf7b875b9101b425bdb1104a134f1
Val Melamed <valo@vtmelamed.com>
2026-04-07 14:22:42 -0400

    refactor: clean up usings and improve test method naming

--- src/UlidType/usings.cs
+++ src/UlidType/usings.cs
@@ -7,18 +7,12 @@ global using System.Diagnostics.CodeAnalysis;
 global using System.Numerics;
 global using System.Security.Cryptography;
 global using System.Text.Json;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Numerics;
 global using System.Runtime.Serialization;
 
-global using BenchmarkDotNet.Attributes;
+global using static System.Buffers.Binary.BinaryPrimitives;
 
 global using vm2;
-global using vm2.SemVerSerialization.NsJson;
-global using vm2.SemVerSerialization.SysJson;
-
 global using vm2.UlidSerialization.NsJson;
 global using vm2.UlidSerialization.SysJson;
 
-global using static System.Buffers.Binary.BinaryPrimitives;
 global using static vm2.Ulid;
