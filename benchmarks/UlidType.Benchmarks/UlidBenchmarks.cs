81a385e454db71a2afcd1ac95417dd3f8549012d
Val Melamed <valo@vtmelamed.com>
2026-04-09 23:54:45 -0400

    chore: update package versions and improve workflow configurations

--- benchmarks/UlidType.Benchmarks/UlidBenchmarks.cs
+++ benchmarks/UlidType.Benchmarks/UlidBenchmarks.cs
@@ -38,9 +38,6 @@ class PreGeneratedData<T>
 #else
 [SimpleJob(RuntimeMoniker.HostProcess)]
 #endif
-[JsonExporterAttribute.BriefCompressed]
-[MarkdownExporterAttribute.GitHub]
-[MemoryDiagnoser]
 public class NewUlid
 {
     [Params(nameof(CryptoRandom), nameof(PseudoRandom))]
@@ -78,9 +75,6 @@ public class NewUlid
 #else
 [SimpleJob(RuntimeMoniker.HostProcess)]
 #endif
-[JsonExporterAttribute.BriefCompressed]
-[MarkdownExporterAttribute.GitHub]
-[MemoryDiagnoser]
 public class UlidToString
 {
     const int MaxDataItems = 1000;
@@ -115,9 +109,6 @@ public class UlidToString
 #else
 [SimpleJob(RuntimeMoniker.HostProcess)]
 #endif
-[JsonExporterAttribute.BriefCompressed]
-[MarkdownExporterAttribute.GitHub]
-[MemoryDiagnoser]
 public class ParseUlid
 {
     const int MaxDataItems = 1000;
